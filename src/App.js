a1c50ebafba6a4614bb1de85c4de90fcb1275a19
sewerynzagajny <seweryn.zagajny@gmail.com>
2024-11-02 02:46:41 +0100

    02_11_2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const units = [
+  { id: 0, unit: "g" },
+  { id: 1, unit: "kg" },
+  { id: 2, unit: "ml" },
+  { id: 3, unit: "l" },
+];
+
+const genderAge = [
+  { id: 0, gneder: "Mężczyzna" },
+  { id: 1, gneder: "Kobieta" },
+  { id: 2, gneder: "Dziecko" },
+];
+
+function Button({ style, onClick, children }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button style={style} onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  const [foodItems, setFoodItems] = useState([]);
+
+  function handleAddItems(foodItem) {
+    setFoodItems((foodItems) => [...foodItems, foodItem]);
+  }
+  return (
+    <div className="app">
+      <Headline />
+      <AddItemForm />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function Headline() {
+  return (
+    <>
+      <h1 className="headline">Szacuj Kalorie!</h1>
+    </>
+  );
+}
+
+function AddItemForm() {
+  const [food, setFood] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [unit, setUnit] = useState("");
+  const [who, setWho] = useState("");
+
+  return (
+    <form className="add_item">
+      <input
+        id="input-food"
+        className="add_item__food"
+        type="text"
+        placeholder="Danie lub składnik"
+        value={food}
+        onChange={(e) => setFood(e.target.value)}
+      />
+      <div className="add_item__container">
+        <input
+          id="input-quantity"
+          className="add_item__container--quantity"
+          type="number"
+          placeholder="Ilość"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <label
+          className="add_item__container--label"
+          htmlFor="item-select-unit"
+        >
+          Wybierz jednostkę:
+        </label>
+        <select
+          id="item-select-unit"
+          className="add_item__container--select-unit"
+          value={unit}
+          onChange={(e) => setUnit(e.target.value)}
+        >
+          {units.map((el) => (
+            <option value={el.unit} key={el.id}>
+              {el.unit}
+            </option>
+          ))}
+        </select>
+        <select
+          id="item-select-who"
+          className="add_item__container--select-who"
+          value={who}
+          onChange={(e) => setWho(e.target.value)}
+        >
+          {genderAge.map((el) => (
+            <option value={el.gneder} key={el.id}>
+              {el.gneder}
+            </option>
+          ))}
+        </select>
+      </div>
+      <Button style={{ width: "12%" }}>Dodaj</Button>
+    </form>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p className="copyright">
+        Copyright &copy; <span>{new Date().getFullYear()}</span> by Seweryn
+        Zagajny. <br />
+        All rights reserved.
+      </p>
+    </footer>
+  );
+}
